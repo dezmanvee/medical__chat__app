490a09c04a01acc0c330990e761a5aae8b0b7294
Bamidele Ademokoya <bamyonline@gmail.com>
2024-01-28 20:01:34 +0100

    Change localhost server to a hosted/live URL

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -27,7 +27,7 @@ const Auth = () => {
         
         const {username, password, phoneNumber, avatarURL} = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://medlyapp.onrender.com/auth';
 
         const {data: {token, userId, hashedPassword, fullName}} = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
             fullName: form.fullName, username, password, phoneNumber, avatarURL, 
